b4391f45afb0afce1c665d69733943539d449696
harrisol22 <lissy.harrison@gmail.com>
2023-05-09 11:49:57 -0600

    functioning demo, database update wip

--- AsyncMuxDemo/AsyncMuxDemo/ContentView.swift
+++ AsyncMuxDemo/AsyncMuxDemo/ContentView.swift
@@ -14,24 +14,27 @@ private let backgroundURL = URL(string: "https://images.unsplash.com/photo-15130
 
 
 struct ContentView: View {
-
+    
     @State var items: [WeatherItem] = []
     @State var isLoading: Bool = false
-
+    @State var selectedCity: String = ""
+    @State var citiesToDisplay: [String] = []
+    let cities: [String] = ["New York", "London", "Paris", "Tokyo"]
+    
     var body: some View {
         listView()
             .background(backgroundImageView())
             .preferredColorScheme(.dark)
-
-        .serverTask(withAlert: true) {
-            items = try await WeatherAPI.reload(refresh: false)
-        }
-
-        .serverRefreshable(withAlert: false) {
-            items = try await WeatherAPI.reload(refresh: true)
-        }
+        
+            .serverTask(withAlert: true) {
+                items = try await WeatherAPI.reload(refresh: false)
+            }
+        
+            .serverRefreshable(withAlert: false) {
+                items = try await WeatherAPI.reload(refresh: true)
+            }
     }
-
+    
     @MainActor // this is because List() generates an error in strict mode, an Apple bug
     @ViewBuilder
     private func listView() -> some View {
@@ -40,33 +43,54 @@ struct ContentView: View {
                 .overlay(ProgressView())
         }
         else {
-            List {
-                ForEach(items, id: \.self) { item in
+            Picker("Please choose a color", selection: $selectedCity) {
+                ForEach(cities, id: \.self) {
+                    Text($0)
+                }
+            }
+            .onChange(of: selectedCity) { _ in
+                if !citiesToDisplay.contains(selectedCity) {
+                    citiesToDisplay.append(selectedCity)
+                }
+                print(citiesToDisplay)
+            }
+        }
+        List {
+            ForEach(items, id: \.self) { item in
+                if citiesToDisplay.contains(item.place.city) {
                     HStack {
                         Text("\(item.place.city), \(item.place.countryCode)")
                         Spacer()
                         Text(item.weather.map { "\(Int(round($0.currentWeather.temperature)))ºC" } ?? "-")
+                        Button(action: {
+                            items.remove(at:items.firstIndex(of: item)!)
+                            citiesToDisplay.remove(at: citiesToDisplay.firstIndex(of: item.place.city)!)
+                        }, label: {
+                            Text("Delete")
+                        })
                     }
                     .listRowBackground(Color.clear)
                 }
+                
             }
-            .listStyle(.inset)
-            .font(.title2)
-            .scrollContentBackground(.hidden)
         }
-    }
-
-    private func backgroundImageView() -> some View {
-        RemoteImage(url: backgroundURL) { image in
-            image
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .ignoresSafeArea()
-        } placeholder: {
-            Color(UIColor.systemBackground)
-        }
+        .listStyle(.inset)
+        .font(.title2)
+        .scrollContentBackground(.hidden)
     }
 }
+
+private func backgroundImageView() -> some View {
+    RemoteImage(url: backgroundURL) { image in
+        image
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea()
+    } placeholder: {
+        Color(UIColor.systemBackground)
+    }
+}
+
 
 
 struct ContentView_Previews: PreviewProvider {
