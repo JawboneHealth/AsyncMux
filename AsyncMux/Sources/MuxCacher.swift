fc8b4868e1c26a04d9b89cf234850bdbc7271ff7
harrisol22 <lissy.harrison@gmail.com>
2023-05-15 09:59:22 -0600

    added deleteall func

--- AsyncMux/Sources/MuxCacher.swift
+++ AsyncMux/Sources/MuxCacher.swift
@@ -12,27 +12,27 @@ import Foundation
 public class MuxCacher {
     
     private let muxRootDomain = "_Root.Domain"
-
+    
     public static func load<T: Decodable>(domain: String, key: String, type: T.Type) -> T? {
         return MuxDB.shared.load(keyToLoad: domain + "/" + key, type: type)
     }
-
+    
     public static func save<T: Encodable>(_ object: T, domain: String, key: String) {
         MuxDB.shared.save(key: domain + "/" + key, data: object)
     }
-
+    
     public static func delete(domain: String, key: String) {
         MuxDB.shared.delete(keyToDelete: domain + "/" + key);
     }
-
+    
     public static func deleteDomain(_ domain: String) {
-        try? FileManager.default.removeItem(at: cacheDirURL(domain: domain, create: false))
+        MuxDB.shared.deleteAll();
     }
-
+    
     private static func cacheFileURL(domain: String, key: String, create: Bool) -> URL {
         return cacheDirURL(domain: domain, create: create).appendingPathComponent(key).appendingPathExtension("json")
     }
-
+    
     private static func cacheDirURL(domain: String, create: Bool) -> URL {
         let dir = "AsyncMux/" + domain
         return FileManager.default.cachesDirectory(subDirectory: dir, create: create)
--- AsyncMux/Sources/MuxDB.swift
+++ AsyncMux/Sources/MuxDB.swift
@@ -22,9 +22,9 @@ class MuxDB {
     
     private init() {
         db = try! Connection("/Users/lisa/PiTech/all.health/AsyncMux/muxDB.db")
-       do {
-           try db.run(dataTable.create(ifNotExists: true) { table in
-               table.column(key, unique: true)
+        do {
+            try db.run(dataTable.create(ifNotExists: true) { table in
+                table.column(key, unique: true)
                 table.column(data)
             })
             print("Table Created...")
@@ -37,9 +37,9 @@ class MuxDB {
         let json = try! JSONEncoder().encode(data)
         
         let insert = dataTable.insert(or: .replace,
-            self.key <- key,
-            self.data <- json
-            )
+                                      self.key <- key,
+                                      self.data <- json
+        )
         do {
             let rowID = try db.run(insert)
             return rowID
@@ -53,6 +53,14 @@ class MuxDB {
         let itemToDelete = dataTable.filter(key == keyToDelete)
         do {
             try db.run(itemToDelete.delete())
+        } catch {
+            print(error)
+        }
+    }
+    
+    func deleteAll() {
+        do {
+            try db.run(dataTable.delete())
         } catch {
             print(error)
         }
